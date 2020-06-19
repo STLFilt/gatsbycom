0ffb64d36bcd7af86faebbe660edfb5b69d82782
STLFilt <107614989+STLFilt@users.noreply.github.com>
2020-06-19 18:28:36 +0000

    Fixed documentation warnings

--- cuneiformbrowser/browseResults.php
+++ cuneiformbrowser/browseResults.php
@@ -14,4 +14,18 @@ include_once('config.php');
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Results browser
+<title>Results browser</title>
+<link rel="stylesheet" type="text/css" href="styleEditor.css">
+<script src="lib/jquery.min.js"></script>
+</head>
+<body>
+<h2></h2>
+<div id="confusion" style="float:left; margin-right:2em">
+<svg id="svgMaster"></svg>
+<div id="tooltip" ></div>
+</div>
+
+<div id=Annotations style="float:left">
+<?php
+$mainInfo = json_decode(file_get_contents($_SESSION['cuneidemo']["performance"]."mainIndex.json"),true);
+$trainingData =
