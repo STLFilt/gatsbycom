2e59ce3f81e7623cedca15f56409850f98f2713f
STLFilt <107614989+STLFilt@users.noreply.github.com>
2020-11-21 23:06:12 +0000

    update apk

--- cuneiformbrowser/editor.php
+++ cuneiformbrowser/editor.php
@@ -2,4 +2,18 @@
 	session_start();
 }
 	// Call session manager and check if a user is logged in
-	//include('sessionManagement.php')
+	//include('sessionManagement.php');
+	// Config files
+	include_once('config.php');
+	// Set up the Editor and prepare the html functions. Here the $_SESSION['cuneidemo'] variabels are set.
+	include_once('editorBuild.php');
+
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styleEditor.css">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Loading...</title>
+
+<script src="lib/jquery.min.js"></script>
+</h
