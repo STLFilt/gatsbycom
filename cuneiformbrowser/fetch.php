e71948ad94aaf348c11eb023374627037eff82f0
STLFilt <107614989+STLFilt@users.noreply.github.com>
2021-04-23 07:55:55 +0000

    testing grove button

--- cuneiformbrowser/fetch.php
+++ cuneiformbrowser/fetch.php
@@ -1,1 +1,20 @@
-<
+<?php if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+	if($_SESSION['cuneidemo']["enabled"] != true)
+	{
+		exit;
+	}
+
+	include_once('config.php');
+	include_once('logger.php');
+// 	if(isset($_GET["version"]))
+// 	{
+// 		$_SESSION['cuneidemo']["version"] = $_GET["version"];
+// 		echo json_encode(array('done' => true)	);
+// 		exit;
+// 	}
+// 	else
+// 	{
+// 	$xmlImages = simplexml_load_file(_IMAGESLIST_);
+// 	$imageID = 
