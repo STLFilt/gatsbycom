55868c4177ec317218281cd8dbf04bf2e7e0676c
STLFilt <107614989+STLFilt@users.noreply.github.com>
2021-06-01 07:49:59 +0000

    New CV

--- cuneiformbrowser/functions/miscFunctions.js
+++ cuneiformbrowser/functions/miscFunctions.js
@@ -9,3 +9,23 @@ var usedLabels = Array();
 var dictOrdered = Array();
 var fullDictionary;
 var rectHeight = 14;
+var searchTotal = 0;
+var totals = 0;
+var drawn = false;
+numFound = 0;
+found = [];
+
+function startingSetup()
+{
+	dictionaryPrepare();
+	document.getElementById("selection").addEventListener("change",updateData);
+}
+
+function updateData()
+{
+	var sign = document.getElementById("selection").value;
+	var signNum = parseInt(sign);
+	if(typeof(fullDictionary[sign]) == "undefined")
+		document.getElementById("name").innerHTML = "?";
+	else
+		document.getElementById("name").inn
