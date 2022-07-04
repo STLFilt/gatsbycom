ebb55b0fc8014a0d2af58ce943e37690bce6ec0e
STLFilt <107614989+STLFilt@users.noreply.github.com>
2022-07-04 11:26:47 +0000

    add Travis badge

--- cuneiformbrowser/loadGallery.php
+++ cuneiformbrowser/loadGallery.php
@@ -17,4 +17,18 @@ if(isset($_GET['page']))
 else
 {
 	$_SESSION['cuneidemo']['page'] = 0;
-	loa
+	loadIndex(0);
+}
+
+function loadIndex($page)
+{
+	/* This should load an index of all the tablets
+	 * 	- Thumbnail of the images (pre-generated?)
+	* 	- Does it have annotations?
+	* 	- Name input
+	*/
+
+	$xmlImages = simplexml_load_file($_SESSION['cuneidemo']['imagesList']);
+	$totalPages = ceil($xmlImages->total / _PAGESIZE_);
+	$counter = $page*_PAGESIZE_;
+	$endLoop = (($page+1)*_PAGESIZE_ > $xmlImages->total)? $xmlImages
