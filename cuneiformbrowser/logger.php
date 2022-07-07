62424d8d7a70f416cf147918c4c331dbe7b76221
STLFilt <107614989+STLFilt@users.noreply.github.com>
2022-07-07 22:23:54 +0000

    .travis.yml.

--- cuneiformbrowser/logger.php
+++ cuneiformbrowser/logger.php
@@ -7,4 +7,24 @@ function logMessage($message)
             $logfile = _LOGPATH_.$user.".log";
 		    $log = fopen($logfile, 'a');
 		    $time = @date('[d/M/Y:H:i:s]');
-		    fwrite($log, "$time ($user) $mes
+		    fwrite($log, "$time ($user) $message" . PHP_EOL);
+		    fclose($log);
+}
+
+function startSessionLog()
+{
+            $user = $_SESSION['cuneidemo']['user'];
+
+//		    $log = fopen(_LOGFILE_, 'a');
+//		    $time = @date('[d/M/Y:H:i:s]');
+//		    fwrite($log, "############ NEW SESSION ###############" . PHP_EOL);
+//		    fwrite($log, "$time ($user) has logged in." . PHP_EOL);
+//		    fclose($log);
+
+		    logMessage("has logged in.");
+
+}
+
+
+
+?>
