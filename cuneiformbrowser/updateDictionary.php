14e00736a717c810cd43c4d60dc66eab670e8e12
STLFilt <107614989+STLFilt@users.noreply.github.com>
2022-07-17 15:22:30 +0000

    Translated using Weblate (Breton)

--- cuneiformbrowser/updateDictionary.php
+++ cuneiformbrowser/updateDictionary.php
@@ -1,3 +1,17 @@
 <?php
 session_start();
- i
+ if($_SESSION['cuneidemo']["enabled"] != true)
+ {
+ 	echo "nope!";
+ 	exit;
+ }
+include_once('config.php'); // Needed for the different Folders
+include_once('logger.php');
+$thumb = true;
+
+if(isset($_POST['id']))
+{
+	$dictionaryJSON = file_get_contents($_SESSION['cuneidemo']['groupFolder']."dictionary.json");
+	$dictionary = json_decode($dictionaryJSON,true);
+
+		//foreach($_POST['data'] as $newEntry) // Newentry 
