8f09f52fb97139bb50e3ef1539d3e49d02df1d73
STLFilt <107614989+STLFilt@users.noreply.github.com>
2022-07-21 07:46:39 +0000

    Add report-card

--- cuneiformbrowser/updateDictionary.php
+++ cuneiformbrowser/updateDictionary.php
@@ -14,4 +14,19 @@ if(isset($_POST['id']))
 	$dictionaryJSON = file_get_contents($_SESSION['cuneidemo']['groupFolder']."dictionary.json");
 	$dictionary = json_decode($dictionaryJSON,true);
 
-		//foreach($_POST['data'] as $newEntry) // Newentry 
+		//foreach($_POST['data'] as $newEntry) // Newentry = {id: X, label = Y}
+		//{
+		if(is_array($_POST['label']))
+		{
+			$dictionary[$_POST['id']] = $_POST['label'];
+			$thumb = false;
+		}else
+			$dictionary[$_POST['id']][] = $_POST['label'];
+		//}
+
+	$dictionaryJSON = json_encode($dictionary);
+
+	file_put_contents($_SESSION['cuneidemo']['groupFolder']."dictionary.json",$dictionaryJSON);
+
+	// Now, generate a new thumb, if needed
+	if(!file_exists($_SESSION['cuneidemo']['groupModels']."modelThumb/thumb_".spri
